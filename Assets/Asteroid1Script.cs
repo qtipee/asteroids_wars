50e2dd9a1a0d18183d4ca25e2248a4af73ebd951
qtipee <quent.vallat@gmail.com>
2020-02-17 10:08:58 +0100

    GM + Asteroid1 Prefab OnMouseDown

---
+++ Assets/Asteroid1Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid1Script : MonoBehaviour
+{
+	public GameManager GM;
+	
+	public int lifePoints = 5;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	private void OnMouseDown()
+	{
+
+		if (GM.gameIsPlaying)
+		{
+			GM.UpdateScore(1);
+		}
+	}
+}
