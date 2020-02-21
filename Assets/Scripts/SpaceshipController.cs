32cc5af2003b00c842a30b0ef0fb1557beb30cbc
colin.friche <colin.friche@he-arc.ch>
2020-02-21 12:51:18 +0100

    push for Gael

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SpaceshipController : MonoBehaviour
 {
-    private float speed = 10.0f;
+    private float speed = 100.0f;
 
     public float lookSpeed = 10f;
 
     private float _camRotationY;
     private float _camRotationX;
 
-    public Vector2 sensitivity = new Vector2(0.5f, 0.5f);
+    public Vector2 sensitivity = new Vector2(1f, 1f);
     public Vector2 smoothing = new Vector2(3f, 3f);
     public Vector2 clampInDegrees = new Vector2(360, 180);
 
