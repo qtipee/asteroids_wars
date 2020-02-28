f3bc666afcfb35b2b0fe621f37df84e6a3bb1d6a
qtipee <quent.vallat@gmail.com>
2020-02-28 10:33:33 +0100

    MenuScene

---
+++ Assets/Scripts/NumberInputField.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberInputField : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+    public int minValue;
+    public int maxValue;
+    public int defaultValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField.onValueChanged.AddListener(delegate { ValueChanged(); });
+
+        // Default value at start
+        inputField.text = defaultValue.ToString();
+    }
+
+    // On input value changed
+    private void ValueChanged()
+	{
+        int intValue;
+
+        // Tries to parse the string to an integer
+        if (int.TryParse(inputField.text, out intValue))
+		{
+            // Min limit
+            if (intValue < minValue)
+            {
+                inputField.text = minValue.ToString();
+            }
+            // Max limit
+            else if (intValue > maxValue)
+            {
+                inputField.text = maxValue.ToString();
+            }
+        }
+	}
+}
